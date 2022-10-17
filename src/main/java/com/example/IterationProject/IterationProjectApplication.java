a6f581b86f02686dea97dbf8f3bc7c842cc1aee1
Максим Комозин <m.i.komozin@gmail.com>
2022-10-17 09:43:11 +0700

    проект скомбинирован. лобавлены библиотеки jpa и postgresql. инициаоизирован git

---
+++ src/main/java/com/example/IterationProject/IterationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.IterationProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IterationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IterationProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/IterationProject/IterationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IterationProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IterationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
